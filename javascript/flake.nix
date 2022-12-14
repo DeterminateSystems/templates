4358b5c4a0a7dfe238d6c2174a7553876fa4ca9b
Luc Perkins <lucperkins@gmail.com>
2022-12-14 13:34:59 -0300

    Remove flake-utils in all templates

--- javascript/flake.nix
+++ javascript/flake.nix
@@ -3,38 +3,39 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs }:
     let
-      pkgs = import nixpkgs { inherit system; };
+      nameValuePair = name: value: { inherit name value; };
+      genAttrs = names: f: builtins.listToAttrs (map (n: nameValuePair n (f n)) names);
+      allSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
     in
     {
-      packages.default = pkgs.stdenv.mkDerivation {
-        name = "zero-to-nix-javascript";
+      packages = forAllSystems ({ pkgs }: {
+        default = pkgs.stdenv.mkDerivation {
+          name = "zero-to-nix-javascript";
 
-        buildInputs = [
-          pkgs.nodePackages.pnpm
-          pkgs.nodejs-18_x
-        ];
+          buildInputs = [
+            pkgs.nodePackages.pnpm
+            pkgs.nodejs-18_x
+          ];
 
-        src = self;
+          src = ./.;
 
-        buildPhase = ''
-          pnpm install
-          pnpm run build
-        '';
+          buildPhase = ''
+            pnpm install
+            pnpm run build
+          '';
 
-        installPhase = ''
-          mkdir $out
-          cp dist/index.html $out
-        '';
-      };
-    });
+          installPhase = ''
+            mkdir $out
+            cp dist/index.html $out
+          '';
+        };
+      });
+    };
 }
