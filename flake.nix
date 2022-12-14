d9cc9a29a9d898ac20b7f15a534c59890f9610af
Luc Perkins <lucperkins@gmail.com>
2022-12-14 13:41:07 -0300

    Remove flake-utils in root

--- flake.nix
+++ flake.nix
@@ -1,34 +1,45 @@
 {
-  outputs = { self, nixpkgs, flake-utils }: {
-    templates = {
-      go = {
-        path = ./go;
-        description = "Go starter template for Zero to Nix";
-      };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
 
-      javascript = {
-        path = ./javascript;
-        description = "JavaScript starter template for Zero to Nix";
-      };
-
-      python = {
-        path = ./python;
-        description = "Python starter template for Zero to Nix";
-      };
-
-      rust = {
-        path = ./rust;
-        description = "Rust starter template for Zero to Nix";
-      };
-    };
-  } // flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs }:
     let
-      pkgs = import nixpkgs { inherit system; };
-      exampleShells = import ./shell/example.nix { inherit pkgs; };
+      nameValuePair = name: value: { inherit name value; };
+      genAttrs = names: f: builtins.listToAttrs (map (n: nameValuePair n (f n)) names);
+      allSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
     in
     {
-      devShells = {
-        inherit (exampleShells) example javascript python go rust multi;
+      devShells = forAllSystems ({ pkgs }:
+        let
+          exampleShells = import ./shell/example.nix { inherit pkgs; };
+        in
+        {
+          inherit (exampleShells) example javascript python go rust multi;
+        });
+      templates = {
+        go = {
+          path = ./go;
+          description = "Go starter template for Zero to Nix";
+        };
+
+        javascript = {
+          path = ./javascript;
+          description = "JavaScript starter template for Zero to Nix";
+        };
+
+        python = {
+          path = ./python;
+          description = "Python starter template for Zero to Nix";
+        };
+
+        rust = {
+          path = ./rust;
+          description = "Rust starter template for Zero to Nix";
+        };
       };
-    });
+    };
 }
