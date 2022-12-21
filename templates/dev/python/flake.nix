6ca8b874aaca9c39645d26143970aa8f7442945f
Luc Perkins <lucperkins@gmail.com>
2022-12-21 12:43:24 -0300

    Separate into pkg and dev templates

---
+++ templates/dev/python/flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Go example flake for Zero to Nix";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      nameValuePair = name: value: { inherit name value; };
+      genAttrs = names: f: builtins.listToAttrs (map (n: nameValuePair n (f n)) names);
+      allSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [ python311 virtualenv python311Packages.pip ];
+        };
+      });
+    };
+}
